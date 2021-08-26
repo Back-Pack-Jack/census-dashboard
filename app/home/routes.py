bf7df174c5a83fd5db61aae6c058767247140af0
Dimi <36083052+Wesley-E@users.noreply.github.com>
2021-08-26 17:48:24 +0100

    working

--- app/home/routes.py
+++ app/home/routes.py
@@ -22,46 +22,87 @@ class API:
 
     devices = None
 
-    def __init__(self, df=None, uuid='all', datefrom='all', dateto='all', freq='D', sensor=None):
+    def __init__(self, df=None, uuid='all', datefrom='all', dateto='all', freq='D', sensor=None, measure=None, metric=None):
         self.mob_address_uuid = f'http://127.0.0.1:12345/mobility/{uuid}/{datefrom}/{dateto}/{freq}'
-        self.sens_address_uuid = f'http://127.0.0.1:12345/sensor/{df}/{uuid}/{datefrom}/{dateto}/{freq}/{sensor}'
+        self.sens_address_uuid = f'http://127.0.0.1:12345/sensor/{df}/{uuid}/{datefrom}/{dateto}/{freq}/{measure}/{metric}'
         self.devices = f'http://127.0.0.1:12345/devices'
 
-@blueprint.route('/index')
-@login_required
-def index():
-    api = API(datefrom='24/08/2021', dateto='26/08/2021')
+def pull_data(uuid, datefrom, dateto):
+    api = API(datefrom=datefrom, dateto=dateto)
     response = requests.get(api.mob_address_uuid)
     mobility_data = response.json()
     mobility_data = mobility_data['mobility']
     df = pd.read_json(mobility_data)
-    '''
-    mobility_data = api_to_dataframe(mobility_data, 0)
-    bicycle = mobility_data['bicycle']
-    bus = mobility_data['bus']
-    car = mobility_data['car']
-    motorbike = mobility_data['motorbike']
-    person = mobility_data['person']
-    truck = mobility_data['truck']
 
+    bicycle = df.loc[df['classification'] == 'bicycle'].drop(columns='classification')
+    bus = df.loc[df['classification'] == 'bus'].drop(columns='classification')
+    car = df.loc[df['classification'] == 'car'].drop(columns='classification')
+    motorbike = df.loc[df['classification'] == 'motorbike'].drop(columns='classification')
+    person = df.loc[df['classification'] == 'person'].drop(columns='classification')
+    truck = df.loc[df['classification'] == 'truck'].drop(columns='classification')
+
+    mobility = [bicycle, bus, car, motorbike, person, truck]
+
+    api = API(df='sps', datefrom=datefrom, dateto=dateto, measure='pm2_5', metric='mean')
     response = requests.get(api.sens_address_uuid)
-    sensor_data = response.json()
+    pm2_5_mean = response.json()
+    api = API(df='sps', datefrom=datefrom, dateto=dateto, measure='pm10', metric='mean')
+    response = requests.get(api.sens_address_uuid)
+    pm10_mean = response.json()
+
+    sensors = [pm2_5_mean, pm10_mean]
+
+    api = API()
+    response = requests.get(api.devices)
+    devices = response.json()
+
+
+
+    return mobility, sensors
+
+@blueprint.route('/index')
+@login_required
+def index():
+    api = API(datefrom='24-08-2021', dateto='26-08-2021')
+    response = requests.get(api.mob_address_uuid)
+    mobility_data = response.json()
+    mobility_data = mobility_data['mobility']
+    df = pd.read_json(mobility_data)
+
+    bicycle = df.loc[df['classification'] == 'bicycle'].drop(columns='classification')
+    bus = df.loc[df['classification'] == 'bus'].drop(columns='classification')
+    car = df.loc[df['classification'] == 'car'].drop(columns='classification')
+    motorbike = df.loc[df['classification'] == 'motorbike'].drop(columns='classification')
+    person = df.loc[df['classification'] == 'person'].drop(columns='classification')
+    truck = df.loc[df['classification'] == 'truck'].drop(columns='classification')
+
+
+    api = API(df='sps', datefrom='24-08-2021', dateto='26-08-2021', measure='pm2_5', metric='mean')
+    response = requests.get(api.sens_address_uuid)
+    pm2_5 = response.json()
+    api = API(df='sps', datefrom='24-08-2021', dateto='26-08-2021', measure='pm10', metric='mean')
+    response = requests.get(api.sens_address_uuid)
+    pm10 = response.json()
+    
+    print(pm10)
+    '''
     sps = sensor_data['sps']
     sps = api_to_dataframe(sps, 1)
     mics = sensor_data['mics']
     mics = api_to_dataframe(mics, 1)
     zmod = sensor_data['zmod']
     zmod = api_to_dataframe(zmod, 1)
-
+    '''
+    api = API()
     response = requests.get(api.devices)
     devices = response.json()
+    print(devices)
     df = pd.DataFrame(json.loads(devices['devices'])).reset_index()
     API.devices = pd.DataFrame(df.uuid.values, index=df.device_name).to_dict()[0]
     device_locations = API.devices.keys()
     
-    return render_template('index.html', segment='index', bicycle=bicycle, bus=bus, car=car, motorbike=motorbike, person=person, truck=truck, sps=sps, mics=mics, zmod=zmod, device_locations=device_locations)
-    '''
-    return render_template('index.html', segment='index')
+    return render_template('index.html', segment='index', bicycle=bicycle, bus=bus, car=car, motorbike=motorbike, person=person, truck=truck, device_locations=device_locations)
+
 
 
 @blueprint.route('/filter_by_device', methods=['GET', 'POST'])
